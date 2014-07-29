65a5c1a92972ac245984ca11e4fb48b8c5be086e
Luke Bond <luke.n.bond@gmail.com>
2014-07-29 18:27:19 +0100

    Changed message in example

--- example/example.js
+++ example/example.js
@@ -2,7 +2,7 @@ var PPWorker = require('../index');
 
 function workerFn(cb) {
   console.log(Date.now() + ' - Got a request for work');
-  return cb(JSON.stringify({pretending: 'to do work'}));
+  return cb(JSON.stringify({ppworker: 'pretending to do work'}));
 }
 
 var ppw = new PPWorker({url: 'tcp://127.0.0.1:9001'}, workerFn);
