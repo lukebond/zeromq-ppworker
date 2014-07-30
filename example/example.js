c85b80a89aa102912604cbc3869fac7474f64df3
Luke Bond <luke.n.bond@gmail.com>
2014-07-30 13:14:43 +0100

    Updated example

--- example/example.js
+++ example/example.js
@@ -1,8 +1,11 @@
 var PPWorker = require('../index');
 
 function workerFn(cb) {
-  console.log(Date.now() + ' - Got a request for work');
   return cb(JSON.stringify({ppworker: 'pretending to do work'}));
 }
 
-var ppw = new PPWorker({url: 'tcp://127.0.0.1:9001'}, workerFn);
+var ppw = new PPWorker({url: 'tcp://127.0.0.1:9001'}, workerFn)
+  .on('work', function (message) {
+    // this event is emitting for debug/tracing purposes, it's not the worker function!
+    console.log(Date.now() + ' - Sent work to client via queue:', message.toString('utf8'));
+  });
